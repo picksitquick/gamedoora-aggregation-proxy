bd84b3e08d58092c6553c9c7628f11df846c7240
picksitquick <picksitquick@gmail.com>
2024-03-14 11:22:38 +0530

    fixing log path and removing redundant changes

--- src/main/java/com/gamedoora/backend/proxy/aggregation/assembler/StudioDataAggregatorAssembler.java
+++ src/main/java/com/gamedoora/backend/proxy/aggregation/assembler/StudioDataAggregatorAssembler.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
 public class StudioDataAggregatorAssembler {
     private ProducerTemplate producerTemplate;
 
